b822c10de23651bac5fb0a4640bc736b23d5b430
tarathep <bokie.tarathep@gmail.com>
2022-08-15 13:31:13 +0700

    init project

---
+++ src/test/java/com/tarathep/springbear/SpringBearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarathep.springbear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
